da5ef9720018af885582d13cbd97ea776676b6b5
HarshilSevak63 <sevakharshil63@gmail.com>
2025-06-09 13:24:09 +0530

    Delete FSD2 directory

--- FSD2/pugDemo.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.set('viewengine', 'pug');
-app.get('/', (req, res) => {
-    res.render(__dirname + '/pugDemo.pug', { msg: 'Some message', amount: 5000 });
-});
-
-app.listen(5300, () => console.log('Listening at http://localhost:5300'));
